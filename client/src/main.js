fef4da81ccacd9050979fedb09f12ad81e1f4d91
MaksymUsyk <kg.maksym.u@gmail.com>
2023-01-30 14:33:20 +0200

    antd installed

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Antd).mount('#app')
